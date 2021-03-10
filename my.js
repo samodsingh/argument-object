8ecc3fc98ad7c944c6bbbdb10d21c0c3d9400ed7
samod singh <samod0098@gmail.com>
2021-03-10 15:21:50 +0530

    argument object

---
+++ my.js
@@ -0,0 +1,9 @@
+function disp(name1 ,name2) 
+{
+   // arguments change[0] = "umesh";
+  //  document.write(name);
+  document.write(arguments[0] + " ");
+  document.write(arguments[1]);
+}
+disp("samod", "singh"); 
+
